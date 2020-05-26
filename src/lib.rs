5caa857ce036beea00f26ddfd082645f7c8b6c35
Raymond Yeh <extraymond@gmail.com>
2020-05-27 01:32:15 +0800

    1. Header parsed 2. Parameter parsed

--- src/lib.rs
+++ src/lib.rs
@@ -65,151 +65,53 @@ pub struct Header {
     reserved_four: [u8; 44],
 }
 
-pub struct ParameterBlock {
-    header: ParameterHeader,
-    groups: HashMap<u8, Group>,
+pub trait FromReader {
+    fn from_reader<R: Read + Seek>(r: &mut R) -> Self;
 }
 
-#[repr(C, packed)]
-#[derive(Copy, Clone, Debug)]
-pub struct ParameterHeader {
-    reserved_one: u8,
-    reserved_two: u8,
-    parameter_block_counts: u8,
-    magic_number: u8,
-}
-
-// #[derive(Debug)]
-pub struct GroupParse {
-    name_chars_size: u8,
-    id: i8,
-    name: String,
-    offset: i16,
-    desc_chars_size: u8,
-    description: String,
-    params: HashMap<String, Parameter>,
-}
-
-pub struct GroupFormat {
-    // indicates "locked" if value is negative.
-    name_chars_size: i8,
-    id: i8,
-    name: String,
-    offset: i16,
-    desc_chars_size: u8,
-    description: String,
-}
-
-#[derive(Debug)]
-pub struct ParameterFormat {
-    // indicates "locked" if value is negative.
-    name_chars_size: u8,
-    id: i8,
-    name: String,
-    offset: i16,
-    data_length: i8,
-    num_dimensions: u8,
-    dimensions: Vec<u8>,
-    parameter_data: Vec<ParameterData>,
-    desc_chars_size: u8,
-    description: String,
-}
-
-#[derive(Default, Debug)]
-pub struct Group {
-    name: String,
-    description: String,
-    params: HashMap<String, ParameterFormat>,
-}
-
-#[derive(Default, Debug)]
-pub struct Parameter {
-    name: String,
-    description: String,
-}
-
-#[derive(Debug, Clone)]
-enum ParameterData {
-    Character(char),
-    Bytes(u8),
-    Integer(i16),
-    Float(f32),
-}
-
-impl ParameterData {
-    pub fn from_array(data_length: i8, arr: &[u8]) -> Option<Self> {
-        match data_length {
-            -1 => {
-                let content = arr[0] as char;
-                Some(ParameterData::Character(content))
-            }
-            1 => Some(ParameterData::Bytes(arr[0])),
-            2 => {
-                let mut buf = [0_u8; 2];
-                arr.clone().read_exact(&mut buf).unwrap();
-                Some(ParameterData::Integer(i16::from_le_bytes(buf)))
-            }
-            4 => {
-                let mut buf = [0_u8; 4];
-                arr.clone().read_exact(&mut buf).unwrap();
-                Some(ParameterData::Float(f32::from_le_bytes(buf)))
-            }
-            _ => None,
-        }
-    }
-}
-
-pub struct Data {}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_magic() {
-        let mut file = File::open("/home/extraymond/Downloads/takes/001.c3d").unwrap();
-
+impl FromReader for Header {
+    fn from_reader<R: Read + Seek>(r: &mut R) -> Self {
         let mut header: Header = unsafe { mem::zeroed() };
         // dbg!(&header);
         let header_size = mem::size_of::<Header>();
-        assert_eq!(header_size, 512);
-
-        let mut buffer = [0_u8; 512];
-        file.read_exact(&mut buffer).unwrap();
 
         unsafe {
             // directly mutate the memory slice under Header.
             let header_slice =
                 slice::from_raw_parts_mut(&mut header as *mut _ as *mut u8, header_size);
 
-            (&buffer[..]).read_exact(header_slice).unwrap();
+            r.read_exact(header_slice).unwrap();
         }
 
-        file.seek(SeekFrom::Start((header.parameter_start as u64 - 1) * 512))
-            .unwrap();
-        dbg!(header.parameter_start);
+        header
+    }
+}
 
+impl FromReader for ParameterHeader {
+    fn from_reader<R: Read + Seek>(r: &mut R) -> Self {
         let mut parameter: ParameterHeader = unsafe { mem::zeroed() };
-        let mut buffer = [0_u8; 4];
-
-        file.read_exact(&mut buffer).unwrap();
 
         unsafe {
             let parameter_slice = slice::from_raw_parts_mut(&mut parameter as *mut _ as *mut u8, 4);
-            (&buffer[..]).read_exact(parameter_slice).unwrap();
+            r.read_exact(parameter_slice).unwrap();
         }
 
-        assert_eq!(parameter.magic_number, 84);
+        parameter
+    }
+}
 
-        dbg!(&parameter);
+impl FromReader for ParameterBlock {
+    fn from_reader<R: Read + Seek>(r: &mut R) -> Self {
+        let header = ParameterHeader::from_reader(r);
+
         let mut u8_buffer = [0_u8];
         let mut i8_buffer = [0_u8];
         let mut i16_buffer = [0_u8; 2];
         let mut string_buffer: Vec<u8> = vec![];
         let mut parameter_buf: Vec<u8> = vec![];
 
-        parameter_buf.resize(parameter.parameter_block_counts as usize * 512 - 4, 0);
-        file.read_exact(&mut parameter_buf).unwrap();
+        parameter_buf.resize(header.parameter_block_counts as usize * 512 - 4, 0);
+        r.read_exact(&mut parameter_buf).unwrap();
         let mut parameter_block_cursor = Cursor::new(&parameter_buf[..]);
 
         let mut groups = HashMap::<u8, Group>::new();
@@ -319,10 +221,176 @@ mod tests {
             parameter_block_cursor = Cursor::new(&parameter_buf[..]);
         }
 
-        for (k, v) in groups.iter() {
-            for (p_name, p) in v.params.iter() {
-                println!("{}: {}", &v.name, &p_name);
+        let groups: HashMap<String, Group> = groups
+            .into_iter()
+            .map(|(_, v)| (v.name.clone(), v))
+            .collect();
+
+        let parameter_block = ParameterBlock { header, groups };
+
+        parameter_block
+    }
+}
+
+#[derive(Debug)]
+pub struct ParameterBlock {
+    header: ParameterHeader,
+    groups: HashMap<String, Group>,
+}
+
+impl ParameterBlock {
+    pub fn get(&self, key: &str) -> Option<&ParameterFormat> {
+        let split_key: &'static str;
+
+        if key.contains(".") {
+            split_key = ".";
+        } else if key.contains(":") {
+            split_key = ":";
+        } else {
+            return None;
+        }
+
+        let mut iter = key.split(split_key);
+        let group_key = iter.next().unwrap();
+        let param_key = iter.next().unwrap();
+
+        if let Some(group) = self.groups.get(group_key) {
+            if let Some(param) = group.params.get(param_key) {
+                return Some(&param);
             }
         }
+
+        None
+    }
+}
+
+#[repr(C, packed)]
+#[derive(Copy, Clone, Debug)]
+pub struct ParameterHeader {
+    reserved_one: u8,
+    reserved_two: u8,
+    parameter_block_counts: u8,
+    magic_number: u8,
+}
+
+// #[derive(Debug)]
+pub struct GroupParse {
+    name_chars_size: u8,
+    id: i8,
+    name: String,
+    offset: i16,
+    desc_chars_size: u8,
+    description: String,
+    params: HashMap<String, Parameter>,
+}
+
+pub struct GroupFormat {
+    // indicates "locked" if value is negative.
+    name_chars_size: i8,
+    id: i8,
+    name: String,
+    offset: i16,
+    desc_chars_size: u8,
+    description: String,
+}
+
+#[derive(Debug)]
+pub struct ParameterFormat {
+    // indicates "locked" if value is negative.
+    name_chars_size: u8,
+    id: i8,
+    name: String,
+    offset: i16,
+    data_length: i8,
+    num_dimensions: u8,
+    dimensions: Vec<u8>,
+    parameter_data: Vec<ParameterData>,
+    desc_chars_size: u8,
+    description: String,
+}
+
+#[derive(Default, Debug)]
+pub struct Group {
+    name: String,
+    description: String,
+    params: HashMap<String, ParameterFormat>,
+}
+
+#[derive(Default, Debug)]
+pub struct Parameter {
+    name: String,
+    description: String,
+}
+
+#[derive(Debug, Clone)]
+pub enum ParameterData {
+    Character(char),
+    Bytes(u8),
+    Integer(i16),
+    Float(f32),
+}
+
+impl ParameterData {
+    pub fn from_array(data_length: i8, arr: &[u8]) -> Option<Self> {
+        match data_length {
+            -1 => {
+                let content = arr[0] as char;
+                Some(ParameterData::Character(content))
+            }
+            1 => Some(ParameterData::Bytes(arr[0])),
+            2 => {
+                let mut buf = [0_u8; 2];
+                arr.clone().read_exact(&mut buf).unwrap();
+                Some(ParameterData::Integer(i16::from_le_bytes(buf)))
+            }
+            4 => {
+                let mut buf = [0_u8; 4];
+                arr.clone().read_exact(&mut buf).unwrap();
+                Some(ParameterData::Float(f32::from_le_bytes(buf)))
+            }
+            _ => None,
+        }
+    }
+}
+
+pub struct Data {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::Path;
+
+    #[test]
+    fn test_magic() {
+        let user = std::env::var("USER").unwrap();
+        let mut file = File::open(format!("/home/{}/Downloads/takes/001.c3d", user)).unwrap();
+
+        let header = Header::from_reader(&mut file);
+        assert_eq!(header.magic_word, 80);
+
+        let parameter = ParameterHeader::from_reader(&mut file);
+        assert_eq!(parameter.magic_number, 84);
+
+        file.seek(SeekFrom::Current(-4)).unwrap();
+        let parameter_block = ParameterBlock::from_reader(&mut file);
+
+        let param_datas = parameter_block.get("POINT:LABELS").unwrap();
+        let lables = param_datas
+            .parameter_data
+            .chunks_exact(param_datas.dimensions[0] as usize)
+            .map(|arr| {
+                arr.iter()
+                    .map(|v| {
+                        if let ParameterData::Character(c) = v {
+                            Some(c)
+                        } else {
+                            None
+                        }
+                    })
+                    .map(|el| el.unwrap())
+                    .collect::<String>()
+            })
+            .collect::<Vec<String>>();
+        dbg!(lables);
     }
 }
